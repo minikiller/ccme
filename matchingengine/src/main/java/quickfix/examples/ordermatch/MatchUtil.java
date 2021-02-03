d44a60436ac5c92a8ed019ccecb291d90fb5edb4
minikiller <minikiller@qq.com>
2021-02-03 17:52:28 +0800

    增加2个map函数

--- matchingengine/src/main/java/quickfix/examples/ordermatch/MatchUtil.java
+++ matchingengine/src/main/java/quickfix/examples/ordermatch/MatchUtil.java
@@ -28,13 +28,12 @@ public class MatchUtil {
      *
      * @return
      */
-    public static Map<String, List<String>> getAfterMap() {
+    public static Map<String, List<String>> getAfterDoubleMap() {
         Map<String, List<String>> after = new HashMap<>();
         List<String> nameList = Util.getNameList();
         List<String> dateList = Util.getDateList();
 
         List<String> singleList = new ArrayList<>();
-        List<String> doubleList = new ArrayList<>();
         for (String name : nameList) {
             for (String date : dateList) {
                 String str = name + "-" + date;
@@ -52,15 +51,57 @@ public class MatchUtil {
         int j = 0;
         int k = dateList.size();
         for (String name : singleList) {
-            List<String> tmpList = new ArrayList<>();
+            List<String> doubleList = new ArrayList<>();
             for (Map.Entry<Integer, String> entry : map.entrySet()) {
                 if (j < entry.getKey()) {
                     String str = name + "-" + entry.getValue();
                     doubleList.add(str);
-                    tmpList.add(str);
                 }
             }
-            after.put(name, tmpList);
+            after.put(name, doubleList);
+            j++;
+            if (j == k) j = 0;
+        }
+        return after;
+    }
+
+    /**
+     * 返回单脚单的时间后序单脚单列表
+     * 例如 <d1,[d2,d3]>
+     * @return
+     */
+    public static Map<String, List<String>> getAfterSingleMap() {
+        Map<String, List<String>> after = new HashMap<>();
+        List<String> nameList = Util.getNameList();
+        List<String> dateList = Util.getDateList();
+
+        List<String> singleList = new ArrayList<>();
+        for (String name : nameList) {
+            for (String date : dateList) {
+                String str = name + "-" + date;
+                singleList.add(str);
+            }
+        }
+
+        Map<Integer, String> map = new HashMap();
+        int i = 0;
+        for (String date : dateList) {
+            map.put(i, date);
+            i++;
+        }
+
+        int j = 0;
+        int k = dateList.size();
+        for (String name : singleList) {
+            String[] single=name.split("-");
+            List<String> doubleList = new ArrayList<>();
+            for (Map.Entry<Integer, String> entry : map.entrySet()) {
+                if (j < entry.getKey()) {
+                    String str = single[0] + "-" + entry.getValue();
+                    doubleList.add(str);
+                }
+            }
+            after.put(name, doubleList);
             j++;
             if (j == k) j = 0;
         }
@@ -73,13 +114,12 @@ public class MatchUtil {
      *
      * @return
      */
-    public static Map<String, List<String>> getBeforeMap() {
+    public static Map<String, List<String>> getBeforeDoubleMap() {
         Map<String, List<String>> before = new HashMap<>();
         List<String> nameList = Util.getNameList();
         List<String> dateList = Util.getDateList();
 
         List<String> singleList = new ArrayList<>();
-        List<String> doubleList = new ArrayList<>();
         for (String name : nameList) {
             for (String date : dateList) {
                 String str = name + "-" + date;
@@ -117,6 +157,54 @@ public class MatchUtil {
         return before;
     }
 
+    /**
+     * 返回单脚单的时间前序单脚单列表
+     * 例如 <d3,[d2,d1]>
+     * @return
+     */
+    public static Map<String, List<String>> getBeforeSingleMap() {
+        Map<String, List<String>> before = new HashMap<>();
+        List<String> nameList = Util.getNameList();
+        List<String> dateList = Util.getDateList();
+
+        List<String> singleList = new ArrayList<>();
+        for (String name : nameList) {
+            for (String date : dateList) {
+                String str = name + "-" + date;
+                singleList.add(str);
+            }
+        }
+
+        Map<Integer, String> nameMap = new HashMap();
+        int i = 0;
+        for (String date : dateList) {
+            nameMap.put(i, date);
+            i++;
+        }
+
+        Map<String, Integer> map = new HashMap();
+        i = 0;
+        for (String date : dateList) {
+            map.put(date, i);
+            i++;
+        }
+
+        int j = 0;
+        for (String name : singleList) {
+            List<String> tmpList = new ArrayList<>();
+            String[] _str = name.split("-");
+            int index = map.get(_str[1]);
+            while (index - j > 0) {
+                String str = nameMap.get(j);
+                tmpList.add(_str[0] + "-" + str );
+                j++;
+            }
+            before.put(name, tmpList);
+            j = 0;
+        }
+        return before;
+    }
+
     public static void main(String[] param) {
 //        String str = "FMG3-MAR21, FMG3-JUN21, FMG3-SEP21, HEF1-MAR21, HEF1-JUN21, HEF1-SEP21, FMG3-MAR21-JUN21, FMG3-MAR21-SEP21, FMG3-JUN21-SEP21, HEF1-MAR21-JUN21, HEF1-MAR21-SEP21, HEF1-JUN21-SEP21";
 //        String[] strArray = str.split(",");
@@ -124,7 +212,7 @@ public class MatchUtil {
 //        for (int i = 0; i < strArray.length; i++) {
 //            stringList.add(strArray[i]);
 //        }
-        getBeforeMap();
-        getAfterMap();
+        getBeforeDoubleMap();
+        getAfterDoubleMap();
     }
 }
