404d56e39b2c7cae0aea9080ea381a4e9d8e466c
minikiller <minikiller@qq.com>
2021-03-09 20:21:21 +0800

    new6 branch live-deploy*

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -e
+
 git config --global push.default simple # we only want to push one branch — master
 # specify the repo on the live server as a remote repo, and name it 'production'
 # <user> here is the separate user you created for deploying
