c76c84a21d5e9bd1f32c5630de827fd1b43f271e
minikiller <minikiller@qq.com>
2021-03-09 21:55:32 +0800

    new9 branch live-deploy*

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -3,6 +3,6 @@
 git config --global push.default simple # we only want to push one branch — master
 # specify the repo on the live server as a remote repo, and name it 'production'
 # <user> here is the separate user you created for deploying
-git remote add production ssh://root@39.104.54.42/root/project/ccfe
+git remote add production ssh://root@192.119.116.101/root/project/ccme
 git push production  # push our updates
 #mvn clean package
